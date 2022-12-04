dbf4fed933bd0c8fd61e1298295d39219e664786
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2022-12-04 11:24:40 -0600

    Add Day 4

---
+++ day04/solution2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    fstream fs;
+    fs.open("input.txt", ios::in);
+
+    long long res = 0;
+    if (fs.is_open()) {
+        string s;
+        while (getline(fs, s)) {
+            if (s.length() != 0) {
+                int i = 0;
+                string l = "";
+                while (s[i] != '-') {
+                    l += s[i++];
+                }
+                i++;
+                string r = "";
+                while (s[i] != ',') {
+                    r += s[i++];
+                }
+                i++;
+                string l2_ = "";
+                while (s[i] != '-') {
+                    l2_ += s[i++];
+                }
+                i++;
+                string r2_ = "";
+                while (i < s.length()) {
+                    r2_ += s[i++];
+                }
+
+                int l1 = stoi(l), r1 = stoi(r);
+                int l2 = stoi(l2_), r2 = stoi(r2_);
+
+                if ((l1 >= l2 && r2 >= l1) || (r1 >= l2 && r2 >= r1))
+                    res++;
+                else if ((l2 >= l1 && r1 >= l2) || (r2 >= l1 && r1 >= r2))
+                    res++;
+            }
+        }
+    }
+    cout << res << endl;
+    fs.close();
+}
